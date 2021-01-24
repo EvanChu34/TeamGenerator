2deed810a25e631879697ded01bf335fd868f7fb
Evan Chu <echu34@gmail.com>
2021-01-24 17:05:14 -0500

    added in the lib, more mods

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
 const fs = require('fs');
 const http = require('http');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const Manager = require("./pro");
+const Engineer = require("");
+const Intern = require("");
+const ManagerProfile = require("./profiles/Manager.profile");
+const EngineerProfile = require("./profiles/Engineer.profile");
+const InternProfile = require("./profiles/Intern.profile");
+const TeamRoster = require("./profiles/TeamRoster");
+
+
+class application{
+    constructor(){
+        this.database = {
+            
+        }
+    }
+
+    async getEmployeeInfo(){
+        console.log();
+
+        let employeeInfo = 
+            await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "ID please",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Name please",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Email please",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "title",
+                    name: "title",
+                },
+            ]);
+        
+        createEmployee(employeeInfo){
+            let employee;
+            const {id, name, email} = employeeInfo;
+            switch(employeeInfo.title.toLowerCase()){
+
+
+                
+            }
+
+
+        }
+        
+
+
+
+
+        createTeamRoster(){
+            let mangerProfile = '';
+            let engineers = '';
+            let interns = '';
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
